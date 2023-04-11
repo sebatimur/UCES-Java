ee5f3025499f241663af12a974a54fa8f63fe36a
sebatimur <81577915+sebatimur@users.noreply.github.com>
2023-04-11 23:27:49 +0000

    V2

--- Main.java
+++ Main.java
@@ -7,5 +7,12 @@
     cam1.color = "Azul";
     cam1.talle = 39;
     cam1.mostrar();
+
+    Pantalon pan1 = new Pantalon();
+    pan1.marca = "Nike";
+    pan1.color = "Negro";
+    pan1.talle = "L";
+    pan1.modelo = "Corto";
+    pan1.mostrar();
   }
 }
---
+++ Pantalon.java
@@ -0,0 +1,13 @@
+class Pantalon{
+  public String marca;
+  public String color;
+  public String talle;
+  public String modelo;
+
+  void mostrar(){
+    System.out.println("Marca: " +marca);
+    System.out.println("Color: " +color);
+    System.out.println("Talle: " +talle);
+    System.out.println("Modelo: " +modelo);
+  }
+}
