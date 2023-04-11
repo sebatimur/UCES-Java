b24811267f628a598c1d410ccaff0a9b00214835
sebatimur <81577915+sebatimur@users.noreply.github.com>
2023-04-11 23:15:09 +0000

    Initial commit

---
+++ Camisa.java
@@ -0,0 +1,11 @@
+class Camisa{
+  public String marca;
+  public String color;
+  public int talle;
+  
+  void mostrar()  {
+    System.out.println("Marca"+marca);
+    System.out.println("Color"+color);
+    System.out.println("El talle es"+talle);
+  }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+ class Main
+{
+  public static void main(String[] args)
+  {
+    Camisa cam1 = new Camisa();
+    cam1.marca = "Sarkany";
+    cam1.color = "Azul";
+    cam1.talle = 39;
+    cam1.mostrar();
+  }
+}
