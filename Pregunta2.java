20e06c0cef7272b109fcb54b288a29a8f8e39ced
sebatimur <81577915+sebatimur@users.noreply.github.com>
2023-06-27 19:31:14 -0300

    Pregunta2-Parcial2
    Pregunta2-Parcial2

---
+++ Pregunta2.java
@@ -0,0 +1,10 @@
+public class Pregunta2
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            System.out.println("Numero " + i);
+        }
+    }
+}
