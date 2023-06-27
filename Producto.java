78b3dc4b6b5b1b226b9ddf93b143a92c5dae3717
sebatimur <81577915+sebatimur@users.noreply.github.com>
2023-06-27 19:41:40 -0300

    Pregunta5-Parcial2
    Pregunta5-Parcial2

--- Producto.java
+++ Producto.java
@@ -1,41 +1,45 @@
 public class Producto {
-        private String nombre;
-        private double precio;
-        private int cantidad;
+    private String nombre;
+    private String informacion;
+    private double precio;
+    private int cantidad;
 
-        public Producto(String nombre, double precio, int cantidad) {
-            this.nombre = nombre;
-            this.precio = precio;
-            this.cantidad = cantidad;
+    public Producto(String nombre, String informacion,double precio, int cantidad) {
+        this.nombre = nombre;
+        this.informacion = informacion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getInformaicon() {
+        return informacion;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public double precioTotal() {
+        return precio * cantidad;
+    }
+
+    public static void main(String[] args) {
+        Producto[] productos = {
+                new Producto("Juego de Mesa: Uno","Para mayores de 8 Años", 6, 5),
+                new Producto("LEGO: Star Wars","Para edade entre 3 y 8 Años", 12, 7),
+                new Producto("Cartas Truco","Para mayores de 12 años", 2, 18)
+        };
+
+        for (Producto producto : productos) {
+            System.out.println("Nombre: " + producto.getNombre());
+            System.out.println("Nombre: " + producto.getInformaicon());
+            System.out.println("Cantidad: " + producto.getCantidad());
+            System.out.println("Valor total: " + producto.precioTotal());
+            System.out.println();
         }
-
-        public String getNombre() {
-            return nombre;
-        }
-
-        public double getPrecio() {
-            return precio;
-        }
-
-        public int getCantidad() {
-            return cantidad;
-        }
-
-        public double getValorTotal() {
-            return precio * cantidad;
-        }
-
-        public static void main(String[] args) {
-            Producto[] productos = {
-                    new Producto("Producto 1", 10.99, 2),
-                    new Producto("Producto 2", 5.49, 5),
-                    new Producto("Producto 3", 2.99, 10)
-            };
-
-            for (Producto producto : productos) {
-                System.out.println("Nombre: " + producto.getNombre());
-                System.out.println("Valor total: " + producto.getValorTotal());
-                System.out.println();
-            }
-        }
+    }
 }
